373f8421368a2ef878100d2edfdd4c3aefdafaff
imeanmybabyboy <pushenko.roman@gmail.com>
2025-10-27 18:45:29 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_PV411.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
